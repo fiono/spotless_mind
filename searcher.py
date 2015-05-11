2ee32cd2464df13728134de15d76384df221b25c
Fiona Condon <frcondon@gmail.com>
2015-05-10 23:40:58 -0400

    Parsing phrase queries in searcher

--- searcher.py
+++ searcher.py
@@ -1,52 +1,58 @@
 import sys
 import json
+import re
 from collections import OrderedDict
 
 import posting_utils
 from stemmer import normalize
 
-if (len(sys.argv) != 3):
-    print("Must supply an index name and one quoted search term or phrase.")
+# searches an index file for a query
+def search(query, full_index):
+    results = []
+
+    id_map = full_index["id_map"]
+    index_dict = full_index["index"]
+
+    is_phrase = (len(query) == 1 and re.search(' ', query[0]) != None)
+    if (is_phrase):
+        terms = [normalize(term) for term in query[0].split()]
+        postings_lists = []
+        for term in terms:
+            sorted_postings = OrderedDict(sorted(index_dict[term].items(), key=lambda t:t[0]))
+            postings_lists.append(sorted_postings)
+
+        results = posting_utils.phrase_merge(postings_lists)
+
+    else:
+        terms = [normalize(term) for term in query]
+        docset = []
+        for term in terms:
+            try:
+                term_map = index_dict[term]
+                doc_ids = sorted(term_map.keys())
+            except KeyError:
+                doc_ids = []
+            docset.append(doc_ids)
+
+        results = posting_utils.merge(docset)
+
+    return results
+
+if (len(sys.argv) < 2):
+    print("Must supply a query term or phrase.")
     sys.exit()
 
-index_path = 'indexes/%s_index' % sys.argv[1]
-query = sys.argv[2]
-is_phrase_match = True
-
 try:
-    index_file = open(index_path, "r")
+    index_file = open('indexes/test_data_index', "r")
 except FileNotFoundError:
-    print("The index name provided is not valid.")
+    print("The index file is not valid.")
     sys.exit()
 
 full_index = json.loads(index_file.read())
 index_file.close()
 
-id_map = full_index["id_map"]
-index_dict = full_index["index"]
+query = sys.argv[1:]
 
-terms = [normalize(term) for term in query.split()]
-
-results = []
-if (is_phrase_match):
-    postings_lists = []
-    for term in terms:
-        sorted_postings = OrderedDict(sorted(index_dict[term].items(), key=lambda t:t[0]))
-        postings_lists.append(sorted_postings)
-
-    results = posting_utils.phrase_merge(postings_lists)
-
-else:
-    docset = []
-    for term in terms:
-        try:
-            term_map = index_dict[term]
-            doc_ids = sorted(term_map.keys())
-        except KeyError:
-            doc_ids = []
-        docset.append(doc_ids)
-
-    results = posting_utils.merge(docset)
-
+results = search(query, full_index)
 for doc in results:
-    print(id_map[str(doc)])
+    print(full_index["id_map"][str(doc)])
