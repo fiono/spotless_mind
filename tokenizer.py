ffcbcbdf0c168fdbc67da41949bd2b9ed8c65fa9
Fiona Condon <frcondon@gmail.com>
2015-05-15 17:36:59 -0400

    Adding tokenizer mod to drop stopwords

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,28 @@
+import re
+
+blacklist = {
+        "acetone"   : 0,
+        "topcoat"   : 0,
+        "basecoat"  : 0,
+        "essie"     : 3,
+        "opi"       : 3,
+}
+
+def normalize(token):
+    return re.sub(r'\W+', '', token).lower()
+
 def tokenize(fh):
-    pos = 0
+    blackout_counter = 0
+
     for line in fh:
         tokens = line.split()
         for token in tokens:
-            token = token.lower()
-            yield(pos, token)
-            pos += 1
+            token = normalize(token)
+
+            if (token in blacklist):
+                blackout_counter = blacklist[token]
+            else:
+                if (blackout_counter > 0):
+                    blackout_counter = blackout_counter - 1
+                else:
+                    yield token
