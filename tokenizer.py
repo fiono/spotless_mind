0a9fc0b22e5d2e3a2f1fa7288498f71fdb49e115
fiono <frcondon@gmail.com>
2017-04-16 16:45:23 -0400

    Simplifying tokenizer a bit

--- tokenizer.py
+++ tokenizer.py
@@ -1,30 +1,28 @@
 import re
 
 blacklist = {
-        "acetone"   : 0,
-        "topcoat"   : 0,
-        "basecoat"  : 0,
-        "essie"     : 3,
-        "opi"       : 3,
+    "acetone"   : 0,
+    "topcoat"   : 0,
+    "basecoat"  : 0,
+    "essie"   : 3,
+    "opi"     : 3,
 }
 
 def normalize(token):
-    return re.sub(r'\W+', '', token).lower()
+  return re.sub(r'\W+', '', token).lower()
 
 def tokenize(fh):
-    pos = 0
-    blackout_counter = 0
+  pos = 0
+  blackoutCounter = 0
 
-    for line in fh:
-        tokens = line.split()
-        for token in tokens:
-            token = normalize(token)
+  for line in fh:
+    for token in line.split():
+      token = normalize(token)
 
-            if (token in blacklist):
-                blackout_counter = blacklist[token]
-            else:
-                if (blackout_counter > 0):
-                    blackout_counter = blackout_counter - 1
-                else:
-                    yield (pos, token)
-            pos += 1
+      if (token in blacklist):
+        blackoutCounter = blacklist[token]
+      elif (blackoutCounter > 0):
+        blackoutCounter -= 1
+      else:
+        yield (pos, token)
+      pos += 1
